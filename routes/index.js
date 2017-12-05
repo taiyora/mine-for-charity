90143a979c67056bc7fa0e54d164fcdd9c84aaac
jl-eiki <jl-eiki@protonmail.com>
2017-12-05 18:56:55 +1300

    Add Charities box and FAQ page

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,8 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: website_name });
 });
 
+router.get('/faq', function(req, res, next) {
+    res.render('faq', { title: website_name + ' - FAQ'});
+});
+
 module.exports = router;
